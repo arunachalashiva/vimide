2d24cbba77021f8e2ac6c07bf6ee05648f9b6016
arunachalashiva <srikant@DESKTOP-KFMFE86.>
2025-04-13 11:22:27 +0000

    Minor settings

--- neovim_2025/init.lua
+++ neovim_2025/init.lua
@@ -16,4 +16,13 @@ vim.keymap.set("n", "<Leader>ho", function()
 	vim.lsp.buf.hover()
 end)
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = { "*" },
+	callback = function()
+		if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			vim.api.nvim_exec("normal! g'\"", false)
+		end
+	end,
+})
+
 require("config.lazy")
